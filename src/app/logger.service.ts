4382d24a7bdd1b3c7b31125a872f09d4832017ae
Saul Winer <karatesaul@me.com>
2023-06-25 03:16:11 -0700

    Add environment configs and logger service

---
+++ src/app/logger.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoggerService {
+
+  constructor() { }
+
+  debug(...args: any[]) {
+    if (environment.production) {
+      return;
+    }
+
+    console.debug(args);
+  }
+
+  log(...args: any[]) {
+    if (environment.production) {
+      return;
+    }
+
+    console.log(args);
+  }
+
+  warn(...args: any[]) {
+    console.warn(args);
+  }
+
+  error(...args: any[]) {
+    console.error(args);
+  }
+}
