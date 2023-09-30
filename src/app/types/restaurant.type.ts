7e619e2fd449e50acf2963c6d5cdf2c23c1dfed5
Saul Winer <karatesaul@me.com>
2023-09-30 03:00:09 -0700

    Add Tags to Restaurants

--- src/app/types/restaurant.type.ts
+++ src/app/types/restaurant.type.ts
@@ -1,4 +1,9 @@
+import { Tag } from "./tag.type";
+
 export interface Restaurant {
   id?: number; // Primary key, optional to allow Dexie to autoincrement.
   name: string;
+  tags: Tag[];
 }
+
+export interface CreateRestaurantPayload extends Omit<Restaurant, 'id'> {}
