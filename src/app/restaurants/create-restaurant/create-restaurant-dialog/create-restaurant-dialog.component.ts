9d18125402c5e491a9ebd1af5e258121649249c9
Saul Winer <karatesaul@me.com>
2023-10-02 17:56:46 -0700

    Prefill existing tags in Create Restaurant dialog

--- src/app/restaurants/create-restaurant/create-restaurant-dialog/create-restaurant-dialog.component.ts
+++ src/app/restaurants/create-restaurant/create-restaurant-dialog/create-restaurant-dialog.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnInit, Signal, ViewChild, WritableSignal, computed, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { MatAutocompleteModule, MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
@@ -9,6 +9,8 @@ import { MatDialogModule } from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon';
 import { MatChipInputEvent, MatChipsModule } from '@angular/material/chips';
 import { CreateRestaurantPayload } from '../../../types/restaurant.type';
+import { TagsService } from '../../../data/tags.service';
+import { Tag } from '../../../types/tag.type';
 
 @Component({
   selector: 'app-create-restaurant-dialog',
@@ -27,40 +29,49 @@ import { CreateRestaurantPayload } from '../../../types/restaurant.type';
   templateUrl: './create-restaurant-dialog.component.html',
   styleUrls: ['./create-restaurant-dialog.component.scss']
 })
-export class CreateRestaurantDialogComponent {
-  existingTags: string[] = [];
+export class CreateRestaurantDialogComponent implements OnInit {
+  existingTags: WritableSignal<Tag[]> = signal([]);
+  filteredTags: Signal<Tag[]> = computed(() => this.filterTags());
+  filterValue: WritableSignal<string> = signal('');
   form: FormGroup;
   name: FormControl = new FormControl();
-  tags: string[] = [];
+  tags: Tag[] = [];
   tagsCtrl: FormControl = new FormControl();
   @ViewChild('tagsInput') tagsInput!: ElementRef<HTMLInputElement>;
 
-  constructor() {
+  constructor(private readonly tagsService: TagsService) {
     this.form = new FormGroup({
       name: this.name,
       tags: this.tagsCtrl
     });
   }
 
+  ngOnInit(): void {
+    this.tagsService.list().subscribe(data => this.existingTags.set(data));
+    this.tagsCtrl.valueChanges.subscribe(value => this.filterValue.set(value));
+  }
+
   add(event: MatChipInputEvent): void {
     const value: string = (event.value || '').trim();
 
-    if (value) {
-      this.tags.push(value);
-    }
+    console.warn('No pre-existing tag with name', value);
 
     event.chipInput.clear();
     this.tagsCtrl.setValue(null);
   }
 
+  filterTags(): Tag[] {
+    return this.existingTags().filter((tag: Tag) => tag.value.includes(this.filterValue()))
+  }
+
   getRestaurantPayload(): CreateRestaurantPayload {
     return {
       ...this.form.value,
-      tags: this.tags
-    }
+      tags: this.tags.map((tag: Tag) => tag.id)
+    } as CreateRestaurantPayload
   }
 
-  remove(tag: string): void {
+  remove(tag: Tag): void {
     const index: number = this.tags.indexOf(tag);
 
     if (index >= 0) {
@@ -69,7 +80,13 @@ export class CreateRestaurantDialogComponent {
   }
 
   selected(event: MatAutocompleteSelectedEvent): void {
-    this.tags.push(event.option.viewValue);
+    const selectedTag: Tag | undefined = this.existingTags().find((tag: Tag) => tag.id === event.option.value);
+
+    if (!selectedTag) {
+      throw new Error('Tag not found');
+    }
+
+    this.tags.push(selectedTag);
     this.tagsInput.nativeElement.value = '';
     this.tagsCtrl.setValue(null);
   }
--- src/app/types/restaurant.type.ts
+++ src/app/types/restaurant.type.ts
@@ -3,7 +3,7 @@ import { Tag } from "./tag.type";
 export interface Restaurant {
   id?: number; // Primary key, optional to allow Dexie to autoincrement.
   name: string;
-  tags: Tag[];
+  tags: Tag['id'][];
 }
 
 export interface CreateRestaurantPayload extends Omit<Restaurant, 'id'> {}
