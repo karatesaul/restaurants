538f187f77c21c9ba976fdd10b6788463166914c
Saul Winer <karatesaul@me.com>
2024-01-22 14:12:04 -0800

    Fill in coverage for CreateRestaurantComponent

--- src/app/create-restaurant/create-restaurant.component.spec.ts
+++ src/app/create-restaurant/create-restaurant.component.spec.ts
@@ -1,13 +1,29 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { of } from 'rxjs';
+import CreateRestaurantDialogComponent from '../create-restaurant-dialog/create-restaurant-dialog.component';
+import RestaurantService from '../data/restaurant.service';
+import { CreateRestaurantPayload } from '../types/restaurant.type';
 import CreateRestaurantComponent from './create-restaurant.component';
 
 describe('CreateRestaurantComponent', () => {
   let component: CreateRestaurantComponent;
   let fixture: ComponentFixture<CreateRestaurantComponent>;
+  let mockDialog: jasmine.SpyObj<MatDialog>;
+  let mockDialogRef: jasmine.SpyObj<MatDialogRef<CreateRestaurantDialogComponent>>;
+  let mockRoute: jasmine.SpyObj<ActivatedRoute>;
 
   beforeEach(() => {
+    mockDialogRef = jasmine.createSpyObj({
+      afterClosed: of(null)
+    });
+    mockDialog = jasmine.createSpyObj({
+      open: mockDialogRef
+    });
+    mockRoute = jasmine.createSpyObj(['']);
+
     TestBed.configureTestingModule({
       imports: [
         CreateRestaurantComponent,
@@ -15,15 +31,68 @@ describe('CreateRestaurantComponent', () => {
       ],
       providers: [{
         provide: ActivatedRoute,
-        useValue: {}
+        useValue: mockRoute
       }]
+    }).overrideComponent(CreateRestaurantComponent, {
+      set: {
+        providers: [{
+          provide: MatDialog,
+          useValue: mockDialog
+        }]
+      }
     });
     fixture = TestBed.createComponent(CreateRestaurantComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
+    fixture.detectChanges();
+
     expect(component).toBeTruthy();
+  });
+
+  describe('ngOnInit()', () => {
+    it('opens a CreateRestaurantDialog', () => {
+      fixture.detectChanges();
+
+      expect(mockDialog.open).toHaveBeenCalledOnceWith(CreateRestaurantDialogComponent, {
+        minWidth: 500
+      });
+    });
+
+    describe('when the dialog is closed', () => {
+      let createSpy: jasmine.Spy;
+
+      beforeEach(() => {
+        createSpy = spyOn(TestBed.inject(RestaurantService), 'create').and.returnValue(of(0));
+      });
+
+      it('does nothing if there is no data returned', () => {
+        fixture.detectChanges();
+
+        expect(createSpy).not.toHaveBeenCalled();
+      });
+
+      it('creates a restaurant with the data returned', () => {
+        const payload: CreateRestaurantPayload = {
+          name: 'test',
+          tags: []
+        };
+        mockDialogRef.afterClosed.and.returnValue(of(payload));
+
+        fixture.detectChanges();
+
+        expect(createSpy).toHaveBeenCalledOnceWith(payload);
+      });
+
+      it('navigates to the parent route', () => {
+        const router: Router = TestBed.inject(Router);
+        spyOn(router, 'navigate');
+
+        fixture.detectChanges();
+
+        expect(router.navigate).toHaveBeenCalledOnceWith(['..'], { relativeTo: mockRoute });
+      });
+    });
   });
 });
